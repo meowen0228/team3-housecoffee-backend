2ccdc8d668db0db5a6940179a1edb27d5df6c3be
Meowen <meowen0228@gmail.com>
2022-06-13 14:24:31 +0800

    Wen localhost

--- modules/mysql_config.js
+++ modules/mysql_config.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'us-cdbr-east-05.cleardb.net',
-    user:'bb846d709d3792',
-    password:'3bc574b6',
-    database:'heroku_733e1f02961bde7',
+    // host:'us-cdbr-east-05.cleardb.net',
+    // user:'bb846d709d3792',
+    // password:'3bc574b6',
+    // database:'heroku_733e1f02961bde7',
+    host:'localhost',
+    user:'root',
+    password:'12250710',
+    database:'team3',
     connectionLimit:10,
     waitForConnections:true,
     dateStrings: true
